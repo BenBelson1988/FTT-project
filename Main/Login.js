b0e32a2147eaa0379a85b9407b4c32c5fcc8b255
Ben <belson1988@gmail.com>
2021-10-31 17:31:16 +0200

    before eldan

--- Main/Login.js
+++ Main/Login.js
@@ -1,5 +1,21 @@
 "use strict";
 
-const myfunc = () => {
-  document.getElementById("mainh1").innerHTML = "eldan";
+const tabs = document.querySelector(".wrapper");
+const tabButton = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".content");
+
+tabs.onclick = (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    tabButton.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
 };
