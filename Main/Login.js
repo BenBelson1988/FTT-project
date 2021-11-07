edb9af30895fc7e867f4587576f2d5007d75c922
Ben <belson1988@gmail.com>
2021-11-08 00:48:07 +0200

    ben little changes

--- Main/Login.js
+++ Main/Login.js
@@ -1,4 +1,6 @@
 "use strict";
+localStorage.clear();
+const myStorage = window.localStorage;
 
 const tabs = document.querySelector(".wrapper");
 const tabButton = document.querySelectorAll(".tab-button");
@@ -22,9 +24,21 @@ tabs.onclick = (e) => {
 
 const doctorLogIn = (e) => {
   e.preventDefault();
-  // window.open("ForgetPassword.html", "_self");
-  console.log(document.getElementById("doctorForm").elements[0].value);
-  console.log(document.getElementById("doctorForm").elements[1].value);
+
+  let licenseNumber = document.getElementById("doctorForm").elements[0].value;
+  let doctorPassword = document.getElementById("doctorForm").elements[1].value;
+
+  if (licenseNumber === "") {
+    document.getElementById("licenseError").innerHTML =
+      "Please fill license number.";
+    return;
+  }
+
+  if (doctorPassword === "") {
+    document.getElementById("doctorPasswordError").innerHTML =
+      "Please fill password.";
+    return;
+  }
 
   fetch("../JSON/login.json")
     .then(function (response) {
@@ -32,5 +46,25 @@ const doctorLogIn = (e) => {
     })
     .then(function (data) {
       console.log(data);
+      data.doctors.forEach((element, index) => {
+        if (licenseNumber === element.licenseNumber);
+        else {
+          document.getElementById("licenseError").innerHTML =
+            "The entered license number doesn't exist.";
+          return;
+        }
+        if (data.doctors[index].password === doctorPassword) {
+          window.open("PatientSerachDetails.html", "_self");
+          localStorage.setItem("ActiveUser", data.doctors[index].name);
+          console.log(localStorage);
+        } else {
+          document.getElementById("doctorPasswordError").innerHTML =
+            "Wrong password, Please try again.";
+        }
+      });
     });
+};
+
+const onInputHandler = (error) => {
+  document.getElementById(error).innerHTML = "";
 };
